3f0f29eaa02971691a28fa92953fc17a1ed86b9a
FORNER Yann <yann.forner@etu.univ-amu.fr>
2020-03-03 17:44:09 +0100

    admin connexsion + cookie fini

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 var express = require("express");
 var mysql = require('mysql');
 var mustache = require('mustache-express');
+var session = require('express-session');
 var app = express();
 
 
 app.use(express.urlencoded());
+app.use(session({
+    secret: 'My super session secret',
+    cookie: {
+        httpOnly: true,
+        secure: true,
+        maxAge: 10
+    }
+
+})
+);
 
 app.use(express.static('.'));
 
@@ -21,7 +32,11 @@ app.set('views', './views');
 
 
 app.get('/',(req,res) => {
-    res.render('index',{});
+    if(session.admin === undefined){
+        session.admin = false;
+    }
+    console.log("admin : "+session.admin)
+     res.render('index',{});
 });
 
 app.get('/videos',(req,res) => {
@@ -40,7 +55,9 @@ app.get('/detailVid/:id',(req,res) => {
     });
 
 });
-
+app.get('/solution/:id',(req,res)=>{
+    res.redirect("/");
+});
 app.post('/solution/:id',(req,res)=>{
     sql.query('SELECT * FROM videos WHERE id = ? AND codeVid = ?',[req.params.id,req.body.monCode] , (err, result, fields) => {
         if (err) throw err;
@@ -49,5 +66,29 @@ app.post('/solution/:id',(req,res)=>{
         else res.render('vidSoluce',myResult[0]);
     });
 
+});
+
+app.get('/co',(req,res) =>{
+    if(session.admin){
+        res.render('decoAdmin');
+    }else{
+        res.render('coAdmin',{isFailed : false});
+    }
+});
+
+
+
+app.post('/connectAdmin',(req,res)=>{
+    if(req.body.ident === "test" && req.body.ident === "test"){
+        session.admin=true;
+        res.redirect("/");
+    }else{
+        res.render("coAdmin",{isFailed : true});
+    }
+});
+
+app.get('/disconnectAdmin',(req,res)=>{
+   session.admin=false;
+   res.redirect("/");
 });
 app.listen(3000, () => console.log('movie server at http://localhost:3000'));
