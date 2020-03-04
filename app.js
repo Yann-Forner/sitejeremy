39870f1acceed7f5d21ba6e9e6981b8b3addf031
FORNER Yann <yann.forner@etu.univ-amu.fr>
2020-03-04 14:21:03 +0100

    upload img + bd

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require("express");
 var mysql = require('mysql');
 var mustache = require('mustache-express');
 var session = require('express-session');
+var formidable = require('formidable');
+var fs = require('fs');
 var app = express();
 
 
@@ -43,7 +45,7 @@ app.get('/videos',(req,res) => {
     sql.query('SELECT * FROM videos ', (err, result, fields) => {
         if (err) throw err;
         var myResult = result;
-        res.render('videos',{myResult : myResult});
+        res.render('videos',{myResult : myResult, isAdmin :true});//ATTTENTIIOIZOR?RO?OIr
     });
 });
 app.get('/detailVid/:id',(req,res) => {
@@ -66,6 +68,41 @@ app.post('/solution/:id',(req,res)=>{
         else res.render('vidSoluce',myResult[0]);
     });
 
+});
+
+
+
+app.get('/ajoutVid',(req,res) => {
+    if(session.admin === false){//attention
+        res.render('ajoutVid',{});
+    }else{
+        res.redirect("/");
+    }
+
+});
+
+app.post('/ajoutVidConfirm',(req,res) => {
+    var form = new formidable.IncomingForm();
+    var query = "INSERT INTO videos VALUES (null,?,'','',?,'')";
+
+
+
+    form.parse(req, function (err, fields, files) {
+        sql.query(query,[fields.titre,fields.code], function (err, result) {
+            if (err) throw err;
+        var oldpath = files.upImg.path;
+        var newpath = 'C:/Users/Yann/Desktop/sites/sitejeremy/img/' + "img"+result.insertId+".png";
+
+            sql.query("UPDATE videos SET lienVid = ? , lienSol = ?, lienImg = ? WHERE id = ?",["vid"+result.insertId+".mp4","sol"+result.insertId+".mp4","img"+result.insertId+".png",result.insertId], function (err, result) { if (err) throw err;});
+
+
+        fs.rename(oldpath, newpath, function (err) {
+            if (err) throw err;
+            res.write('File uploaded and moved!');
+            res.end();
+        });
+        });
+    });
 });
 
 app.get('/co',(req,res) =>{
@@ -91,4 +128,5 @@ app.get('/disconnectAdmin',(req,res)=>{
    session.admin=false;
    res.redirect("/");
 });
+
 app.listen(3000, () => console.log('movie server at http://localhost:3000'));
